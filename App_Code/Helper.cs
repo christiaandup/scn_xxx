b8673ecba7f5aa91806cbe2d2efe1eb94ff737c9
christiaandup <christiaan@koster.co.za>
2015-08-07 14:16:20 +0200

    V2

---
+++ App_Code/Helper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Helper
+/// </summary>
+public static class Helper
+{
+    public static IEnumerable<T> Add<T>(this IEnumerable<T> e, T value)
+    {
+        foreach (var cur in e)
+        {
+            yield return cur;
+        }
+        yield return value;
+    }
+}
